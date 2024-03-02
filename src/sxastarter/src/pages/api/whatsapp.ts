88533e6de64ace565c7f71b01652858b19a037a8
sandeepperficient <sandeepkumar.gupta@perficient.com>
2024-03-02 23:57:04 +0530

    Removed the Bearer token
    Removed the Bearer token

--- src/sxastarter/src/pages/api/whatsapp.ts
+++ src/sxastarter/src/pages/api/whatsapp.ts
@@ -3,7 +3,7 @@ export async function sendWhatsAppMessage(to: string, message: string) {
     const response = await fetch('https://graph.facebook.com/v18.0/197028526837376/messages', {
       method: 'POST',
       headers: {
-        'Authorization': 'Bearer EAATXUkWOVCkBO0cV2zDF2ZC6Wfj5UcDCdqDtzJctImGOgoGSqV7WhQJ8ZA56ffAUYD0N84oaCFy1BAZAmSdI0eL4ZCMvap7mo1ZC7ZAq4P7v3WB2PzmhGJjYo5ST2G1SJFh7OgQaIIXt4JnRMl0CzjQnJpR4ZBg1sDiqppRGqtVKl2srwoTAxvf6ZCiwZCWEGCfkyox3xadk1Lf8La0I1OzAMViqw3YguiG4ZD',
+        'Authorization': 'Bearer PROVIDE_Bearer_TOKEN_HERE',
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
